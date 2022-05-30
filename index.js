b71eeecb638d4f8b99eaad26d0411d97fb349885
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-05-30 21:40:40 +0930

    REACT remark

---
+++ index.js
@@ -0,0 +1,4 @@
+// ROOT LEVEL1
+// REACT NPM - https://www.npmjs.com/package/react
+// CHECK OUT OFFICIAL ADVANTAGE HERE - https://reactjs.org/docs/rendering-elements.html#react-only-updates-whats-necessary
+// react component- not changeable & Readonly prop -https://reactjs.org/docs/components-and-props.html#props-are-read-only
---
+++ mappage.jsx
@@ -0,0 +1,2 @@
+// 
+// WHAT THE F**K IS JSX? -https://zh-hans.reactjs.org/docs/introducing-jsx.html#jsx-represents-objects
